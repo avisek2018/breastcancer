dedab2cb1557d16c089b42bdedda2ee755a054d9
avisek2018 <44280320+avisek2018@users.noreply.github.com>
2019-07-30 18:08:23 -0400

    Add files via upload

---
+++ packages.R
@@ -0,0 +1,16 @@
+#These are the package needed to run the App:
+  
+library(devtools)
+install_github("vqv/ggbiplot")
+
+install.packages("shiny")
+install.packages("shinydashboard")
+install.packages("tidyverse")
+install.packages("DT")
+install.packages("plotly")
+install.packages("shinycssloaders")
+install.packages("reshape2")
+install.packages("ModelMetrics")
+install.packages("class")
+install.packages("caret")
+install.packages("RColorBrewer")
