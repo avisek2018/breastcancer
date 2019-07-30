1228973e5ce93c962e0a687d9030047bbae17cc6
avisek2018 <44280320+avisek2018@users.noreply.github.com>
2019-07-30 18:57:38 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,5 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
+# This is the user-interface definition of a Shiny web application. 
 #
 # Find out more about building applications with Shiny here:
 #
